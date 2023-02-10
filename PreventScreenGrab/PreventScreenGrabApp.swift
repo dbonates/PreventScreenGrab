ad3f3600d4d8d7463460f8fa24bfd5d23321db0a
Daniel Bonates <daniel.bonates@wexinc.com>
2023-02-10 13:06:00 -0300

    Initial Commit

---
+++ PreventScreenGrab/PreventScreenGrabApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreventScreenGrabApp.swift
+//  PreventScreenGrab
+//
+//  Created by Daniel Bonates on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PreventScreenGrabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
